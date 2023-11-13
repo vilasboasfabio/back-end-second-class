bd09cbfae480f95826dec5eb42114c29da028360
vilasboasfabio <vilasboasfabio564@gmail.com>
2023-11-13 09:03:36 -0300

    vish

--- src/server.js
+++ src/server.js
@@ -9,8 +9,41 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    return res.status(200).send({ message: "Welcome to the beginning of nothingness." });
+  const {day} = req.query; //Desestructuring 
+    return res.status(200).send({ message: `Hello ${day}` });
   });
+
+app.get("/:month", (req, res) => {
+  const {month} = req.params; //Desestructuring 
+    return res.status(200).send({ message: `Hello ${month}` });
+  }
+);
+
+app.post("/", (req, res) => {
+  const {name, age} = req.body; //Desestructuring 
+  if (!name || !age) {
+    return res.status(400).send({ message: "Missing datas" });
+  }
+    return res.status(200).send({ message: `Hello ${name} ${age} yers old` });
+  }
+);
+
+app.put("/:id", (req, res) => {
+  const {id} = req.params; //Desestructuring
+  const {name, age} = req.body; //Desestructuring
+  if (!name || !age) {
+    return res.status(400).send({ message: "Missing datas" });
+  }
+    return res.status(200).send({ message: `User ${id} updated` });
+  }
+);
+
+app.delete("/:id", (req, res) => {
+  const {id} = req.params; //Desestructuring
+    return res.status(200).send({ message: `User ${id} deleted` });
+  }
+);
+
 
   app.listen(port, () =>
   console.log(`😶‍🌫️ Server started on http://localhost:${port}`)
